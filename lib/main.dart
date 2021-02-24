ca63a8b437b17d5536afeb2134788030f6e82aea
Ana <analeiarr@gmail.com>
2021-02-23 22:20:38 -0400

    Inicio do projeto

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:consumo_de_serviso_avancado/future.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Home(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
